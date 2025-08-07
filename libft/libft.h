2da330b23f9e4070ff6e225d8c24d5c0738a5390
DanaNA <dana.nour62@gmail.com>
2025-08-07 11:31:48 +0300

    file functions and function as a pointer

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: DaNa <dna2@student.42amman.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 11:12:58 by DaNa              #+#    #+#             */
-/*   Updated: 2025/08/06 11:16:05 by DaNa             ###   ########.fr       */
+/*   Updated: 2025/08/07 11:09:58 by DaNa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include <stddef.h>
 # include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
 # include <stdlib.h>
 # include <limits.h>
 
@@ -46,4 +48,10 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void 	ft_putnbr_fd(int n, int fd);
 #endif
