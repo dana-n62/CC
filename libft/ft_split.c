198cb930e9a0812a54514ac6962951bc32f65172
DanaNA <dana.nour62@gmail.com>
2025-08-08 13:52:39 +0300

    new functions for diles and pointer functions

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,131 +6,78 @@
 /*   By: DaNa <dna2@student.42amman.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:46:44 by DaNa              #+#    #+#             */
-/*   Updated: 2025/08/06 10:46:45 by DaNa             ###   ########.fr       */
+/*   Updated: 2025/08/08 13:43:30 by DaNa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* #include "libft.h"
+#include "libft.h"
 
-// static int	count_strings(const char *s, char c)
-// {
-// 	int	counter;
-// 	int	i;
-// // printf("here");
-// 	counter = 1;
-// 	i = 0;
-// 	while (s[i] != '\0')
-// 	{
-// 		if ((s[i] == c) && (s[i] != s[i + 1]) && (s[i + 1] != '\0'))
-// 		{
-// 			// i++;
-// 			while (s[i] && s[i] != c)
-// 				i++;
-// 			counter++;
-// 		}
-// 		else
-// 			i++;
-// 	}
-// 	return (counter);
-// }
-
-static int	count_strings(const char *s, char c)
+size_t	count_words(const char *string, char c)
 {
-	int	counter;
-	int	i;
+	int		word_flag;
+	size_t	number_of_words;
 
-	counter = 0;
-	i = 0;
-	while (s[i] != '\0')
+	word_flag = 0;
+	number_of_words = 0;
+	while (string)
 	{
-		while(s[i] != '\0' && s[i] != c)
-			i++;
-		if(s[i] != '\0' && s[i] == c && s[i + 1] != c)
+		if (*string != c && word_flag == 0)
 		{
-			counter++;
+			word_flag++;
+			number_of_words++;
+		}
+		else if (*string == c && word_flag > 0)
+			word_flag = 0;
+		string++;
+	}
+	return (number_of_words);
+}
+
+static int	ft_word(char **all, const char *read, char sep)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (*read)
+	{
+		while (*read == sep)
+			read++;
+		count = 0;
+		while (read[count] != sep && read[count])
+			count++;
+		if (count > 0)
+		{
+			all[i] = ft_substr(read, 0, count);
+			if (!all[i])
+				return (0);
 			i++;
 		}
+		read += count;
 	}
-	return (counter + 1);
-}
-static size_t size_of_slot(const char *s, char c)
-{
-	size_t	size;
-
-	size = 0;
-	while (*s != c && *s != '\0')
-	{
-		size++;
-		s++;
-	}
-	return (size);
-}
-
-static	int splitting_function(char **split, const char **s, char c)
-{
-	size_t	size;
-	size_t	size_s;
-
-	size_s = ft_strlen(*s);
-	size = size_of_slot(*s, c);
-	*split = malloc(sizeof(char) * (size + 1));
-	if (!*split)
-	{
-		free(*split);
-		return(0);
-	}
-	ft_strlcpy(*split, *s, size + 1);
-	if (ft_strlen(*s + size) > size_s)
-		*s += size_s;
-	else
-		*s += size;
 	return (1);
-}
-
-static char *clean_heap(char ***array, int i)
-{
-	while (i >= 0)
-		free(*array[i]);
-	free(**array);
-	return (NULL);
 }
 
 char	**ft_split(const char *s, char c)
 {
-	char	**split;
-	int	number_of_strings;
-	int	i;
-		printf("here\n");
+	char	**strings_array;
+	size_t	i;
+	size_t	words_count;
+
 	if (!s)
 		return (NULL);
-	number_of_strings = count_strings(s, c);
-	split = malloc(sizeof(char *) * (number_of_strings + 1));
-	if (!split)
+	words_count = count_words(s, c);
+	strings_array = malloc(sizeof(char *) * (words_count + 1));
+	if (!strings_array)
 		return (NULL);
- 	while (i < number_of_strings)
+	strings_array[words_count] = NULL;
+	if (!ft_word(strings_array, s, c))
 	{
-		while (s)
-		{
-			if (*s == c && *s != *(s + 1))
-			{
-				if (!splitting_function(&split[i], &s, c))
-					{
-						clean_heap(&split, i);
-						break;
-					}
-			}
-			else
-				s++;
-		}
-		i++;
-		split[i] = NULL;
+		while (i < words_count)
+			free(strings_array[i++]);
+		free(strings_array);
+		return (NULL);
 	}
-	return (split);
+	return (strings_array);
 }
-// int main(void)
-// {
-// 	int count = count_strings("hello dana here", '.');
-// 	printf("%d\n", count);
-// 	return(0);
-// }
- */
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: DaNa <dna2@student.42amman.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:47:07 by DaNa              #+#    #+#             */
-/*   Updated: 2025/08/06 11:11:40 by DaNa             ###   ########.fr       */
+/*   Updated: 2025/08/08 13:50:15 by DaNa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ char	*ft_strnstr(const char *hay_str, const char *needle_str, size_t n)
 {
 	size_t	i;
 	size_t	j;
-	size_t	needle_size;
 
 	i = 0;
 	j = 0;
-	needle_size = ft_strlen(needle_str);
 	if (!hay_str || n == 0)
 		return (NULL);
 	if (needle_str[0] == '\0')
@@ -30,7 +28,7 @@ char	*ft_strnstr(const char *hay_str, const char *needle_str, size_t n)
 		j = 0;
 		if (hay_str[i] == needle_str[j])
 		{
-			while ((hay_str[i] == needle_str[j]) && (needle_str[j] != '\0') && i < n)
+			while (hay_str[i] == needle_str[j] && needle_str && i < n)
 			{
 				i++;
 				j++;
@@ -38,7 +36,7 @@ char	*ft_strnstr(const char *hay_str, const char *needle_str, size_t n)
 			if (needle_str[j] == '\0')
 				return ((char *)hay_str + (i - j));
 		}
-		if (n - i < needle_size)
+		if (n - i < ft_strlen(needle_str))
 			break ;
 		else
 			i++;
