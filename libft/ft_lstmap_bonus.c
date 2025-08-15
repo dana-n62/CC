5c89383a68649cc0f815c1f56f0616104158b954
DanaNA <dana.nour62@gmail.com>
2025-08-15 10:34:36 +0300

    fixed the double free in split and lstmap leak

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,26 +6,31 @@
 /*   By: DaNa <dna2@student.42amman.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 09:09:33 by DaNa              #+#    #+#             */
-/*   Updated: 2025/08/15 08:18:02 by DaNa             ###   ########.fr       */
+/*   Updated: 2025/08/15 10:28:59 by DaNa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+//fixed a memory leak concerning the content of the new node
+//by creating the variable content
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*node;
+	void	*content;
 
 	if (!lst || !f)
 		return (NULL);
 	new_list = NULL;
 	while (lst != NULL)
 	{
-		node = ft_lstnew(f(lst->content));
-		if (!node || !f)
+		content = f(lst->content);
+		node = ft_lstnew(content);
+		if (!node)
 		{
-			ft_lstclear(&new_list, del);
+			del(content);
+			if (new_list != NULL)
+				ft_lstclear(&new_list, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, node);
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: DaNa <dna2@student.42amman.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 10:46:44 by DaNa              #+#    #+#             */
-/*   Updated: 2025/08/09 10:46:47 by DaNa             ###   ########.fr       */
+/*   Updated: 2025/08/15 10:03:45 by DaNa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,9 +91,6 @@ char	**ft_split(char const *s, char c)
 		return (NULL);
 	array_of_words[size_of_array] = NULL;
 	if (!split_string(&array_of_words, s, c))
-	{
-		free(array_of_words);
 		return (NULL);
-	}
 	return (array_of_words);
 }
