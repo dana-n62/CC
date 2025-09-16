7779cbe6b80c996b2b49c25fce6188ff945ead6f
DanaNA <dana.nour62@gmail.com>
2025-09-16 13:24:40 +0300

    working on a fix

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: DaNa <dna2@student.42amman.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 12:49:35 by DaNa              #+#    #+#             */
-/*   Updated: 2025/09/14 13:42:51 by DaNa             ###   ########.fr       */
+/*   Updated: 2025/09/16 13:21:01 by DaNa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,13 @@ static void	check_remainder(char **remainder, char **buffer)
 		*remainder = ft_strdup(*buffer);
 }
 
-static char	*search(int fd, char **remainder, char **buffer, ssize_t read_bytes)
+static char	*search(int fd, char **remainder, char **buffer, ssize_t *read_bytes)
 {
 	char	*sub_string;
 	char	*line;
 	char	*temp;
 
-	while (read_bytes > 0)
+	while (*read_bytes > 0)
 	{
 		sub_string = ft_strchr(*remainder, '\n');
 		if (sub_string != NULL)
@@ -63,29 +63,28 @@ static char	*search(int fd, char **remainder, char **buffer, ssize_t read_bytes)
 			free(temp);
 			return (line);
 		}
-		read_bytes = read(fd, *buffer, BUFFER_SIZE);
-		if (read_bytes <= 0)
+		*read_bytes = read(fd, *buffer, BUFFER_SIZE);
+		if (*read_bytes <= 0)
 			break ;
-		(*buffer)[read_bytes] = '\0';
+		(*buffer)[*read_bytes] = '\0';
 		check_remainder(remainder, buffer);
 	}
 	return (NULL);
 }
 
-static char	*start_the_search(int fd, char **remainder, char **buffer)
+static int	start_the_search(char **line, int fd, char **remainder, char **buffer)
 {
 	ssize_t	read_bytes;
-	char	*line;
 
 	read_bytes = read(fd, *buffer, BUFFER_SIZE);
 	if (read_bytes <= 0)
-		return (NULL);
+		return (read_bytes);
 	(*buffer)[read_bytes] = '\0';
 	check_remainder(remainder, buffer);
 	if (*remainder == NULL)
-		return (NULL);
-	line = search(fd, remainder, buffer, read_bytes);
-	return (line);
+		return (read_bytes);
+	*line = search(fd, remainder, buffer, &read_bytes);
+	return (read_bytes);
 }
 
 char	*get_next_line(int fd)
@@ -93,24 +92,25 @@ char	*get_next_line(int fd)
 	char		*found_line;
 	char		*buffer;
 	static char	*remainder;
+	int	flag;
 
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	found_line = start_the_search(fd, &remainder, &buffer);
+	flag = start_the_search(&found_line, fd, &remainder, &buffer);
 	if (found_line == NULL)
 	{
-		if (ft_strlen(remainder) != 0)
-			found_line = ft_strdup(remainder);
-		free(remainder);
-		remainder = NULL;
+		if (flag >= 0 && ft_strlen(remainder) > 0)
+			flag = start_the_search(&found_line, fd, &remainder, &buffer);
+		else
+		{
+			free(remainder);
+			remainder = NULL;
+		}
 	}
-	if (buffer != NULL)
-	{
-		free(buffer);
-		buffer = NULL;
-	}
+	free(buffer);
+	buffer = NULL;
 	return (found_line);
 }
--- GNL/main.c
+++ GNL/main.c
@@ -7,7 +7,11 @@ int	main(void)
 	int	fd;
 	char	*line;
 
+<<<<<<< HEAD
 	fd = open("file1.txt", O_RDONLY);
+=======
+	fd = open("read1.txt", O_RDONLY);
+>>>>>>> d1643e8 (working on a fix)
 	line = get_next_line(fd);
 
 	while (line != NULL)
